aba58ddaf4fff31f7fffa67a15ac9e681ea37096
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-11-23 17:39:35 +0530

    create - header / back button on calander and new task on homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,47 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
 import NewTask from "./NewTask";
-import { useState } from "react";
+
 
 function Header({ createTask }) {
     const [createTaskVisibility, setCreateTaskVisibility] = useState(false);
     const toggleVisibility = () => setCreateTaskVisibility(v => !v);
-
+    const { id } = useParams();
+    const isMobile = useMediaQuery({ maxWidth: 768 })
+    const navigate = useNavigate();
     return (
+
         <>
-            <div className="flex flex-col md:flex-row md:items-center md:justify-between p-5 md:px-10  border-b-3 
+            <div className="flex flex-row md:items-center justify-between p-5 md:px-10  border-b-3 
                 rounded-b-2xl bg-amber-100 my-auto">
-                <Link to="/" className="flex items-center">
+                <div className="flex items-center">
                     <FontAwesomeIcon
                         className="text-3xl bg-yellow-600 py-2 px-1 mr-2 rounded-2xl text-white"
                         icon={faCalendar} />
                     <div className="text-4xl font-bold " >DayX</div>
-                </Link>
-                <div onClick={toggleVisibility}
-                    className="bg-black text-white p-2 rounded-xl flex items-center justify-center mt-3 md:mt-0">
-                    <FontAwesomeIcon icon={faPlus} className="mr-1 text-sm" />
-                    Create New Task
                 </div>
+                {id
+                    ? (<button
+                        onClick={() => navigate('/')}
+                        className="py-2 px-3 rounded-2xl
+                        bg-black hover:bg-gray-900 text-white">
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                        {!isMobile && (<span className="ml-2">Back</span>)}
+                    </button>)
+                    : (<button onClick={toggleVisibility}
+                        className="bg-black text-white p-2 rounded-xl flex items-center justify-center mt-3 md:mt-0">
+                        <FontAwesomeIcon icon={faPlus} className="mr-1 text-sm" />
+                        Create New Task
+                    </button>)
+                }
             </div>
 
             {createTaskVisibility && (
@@ -34,8 +52,6 @@ function Header({ createTask }) {
                 />
             )}
         </>
-
-
     )
 }
 
